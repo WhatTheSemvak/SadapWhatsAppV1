42901994e1c9a0ebbf92b14f142de506d46bae1d
WhatTheSemvak <54854607+WhatTheSemvak@users.noreply.github.com>
2019-09-06 20:32:18 +0700

    SadapWA.sh

---
+++ SadapWA.sh
@@ -0,0 +1,74 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 1
+echo "\033[33;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+figlet "MrHackWA"
+echo "\033[33;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo "\033[32;1m╔═══════════════════════════════════════╗"
+echo "║Author : Mr.®evoer                               "
+echo "║Team   : Moeslim Cyber Attack  X JCA Attacker                  "
+echo "║Pesan  : Jangan Di Recode Ya Tod susah buat nya ajg                 "
+echo "\033[32;1m╚═══════════════════════════════════════╝"
+echo "\033[33;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+sleep 2
+echo "\033[37;1m Mau Sadap WhatsApp Ya Sayang Uuuh Sayang :*"
+sleep 2
+echo "";
+read -p "Masukkan No WhatsApp Kamu Sayang [+62857xxx]" Nomer;
+read -p "Silahakan Masukkan No Targetnya Sayang Contoh [+62857xxx]" Target;
+sleep 2
+echo "\033[34;1m [+]Sedang Scaning..."
+sleep 3
+echo "\033[31;1m [-]Failed..."
+sleep 0.5
+echo "\033[34;1m [+]Sedang Scaning..."
+sleep 1.5
+echo "";
+echo "\033[37;1mSilahkan Login =>Nomer";
+read p;
+sleep 2.5
+echo "Masukkan Target =>Target";
+read p;
+sleep 1.5
+echo "\033[34;1m [+]Sedang Scaning..."
+sleep 4
+echo "\033[34;1m [+]Mohon Tunggu Sebentar"
+sleep 6
+echo "\033[33;1m [+]Succes"
+sleep 3
+echo "\033[34;1mKode Verifikasi=819250"
+sleep 3
+echo "514929"
+sleep 4
+echo "832916"
+sleep 2
+echo "613829"
+sleep 3
+echo "273926"
+sleep 3
+echo "910347"
+sleep 6
+echo "157382"
+sleep 5
+echo "819326"
+sleep 2
+echo "342681"
+sleep 3
+echo "019273"
+sleep 5
+echo "\033[37;1mSilahkan Coba Kode Verifikasi Tersebut..."
+sleep 2
+echo "Terimakasih Telah Memakai Script Ini Sayang :*..."
+sleep 3
+echo "Ketik Exit /Enter Untuk Keluar"
+echo "Author : Ivan Cruzz Revoer"
+read p;
+exit;
